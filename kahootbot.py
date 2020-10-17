30c425e7150e16a8c69e41c0dc2e9eddba08b9a1
PRAVEEN <59436366+praveenp2004@users.noreply.github.com>
2020-10-17 16:32:57 +0530

    Update kahootbot.py

--- kahootbot.py
+++ kahootbot.py
@@ -1,29 +1,35 @@
 from selenium import webdriver
-#import geckodriver_autoinstaller
+# import geckodriver_autoinstaller
 import time
 import random
 from selenium.webdriver.firefox.options import Options
 options = Options()
-options.headless = True
-#path = geckodriver_autoinstaller.install()
-#driver = webdriver.Firefox(executable_path=path, options=options)
+options.headless = False
+# path = geckodriver_autoinstaller.install()
+# driver = webdriver.Firefox(executable_path=path, options=options)
 driver = webdriver.Firefox(options=options)
 
 
 
-id = input("input ur id:")
+
+id = input("input ur id: ")
+a = input("input a fake name: ") 
 driver.get('https://www.kahoot.it')
+
 while True:
     time.sleep(3)
     i = str(random.randrange(1,100))
+    # time.sleep(2)
     search = driver.find_element_by_xpath('''//*[@id="game-input"]''')
+    search.click()
     search.send_keys(id)
     btn = driver.find_element_by_xpath('''//*[@id="root"]/div/div/div/main/div/form/button''')
     btn.click()
-    time.sleep(4)
+    time.sleep(5)
     name = driver.find_element_by_xpath('''//*[@id="nickname"]''')
-    name.send_keys("bots"+i)
-    enter = driver.find_element_by_xpath('''//*[@id="root"]/div/div/div/main/div/form/button''')
+    name.click()
+    name.send_keys(a+i)
+    enter = driver.find_element_by_xpath('''//*[@id="root"]/div/div[1]/div/main/div/form/button''')
     enter.click()
-    driver.execute_script('''window.open("https://www.kahoot.it");''')
-    driver.switch_to.window(driver.window_handles[-1])
+    driver.execute_script('''window.open("https://www.kahoot.it");''') # to a start new tab
+    driver.switch_to.window(driver.window_handles[-1]) # shifting focus to the new tab
